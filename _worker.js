3ab166db467de34975427bf189bdb4738c65a401
Wind-In <149531625+Wind-In@users.noreply.github.com>
2024-06-17 17:39:54 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '108990be-fd83-401f-a9a4-4a45a731c646';
 
-const พร็อกซีไอพีs = ['15.235.167.49'];
+const พร็อกซีไอพีs = ['138.2.104.102'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
